0c9ee387b301d96985d5f8d28684337fd4bd11e1
Cameron Corrado <camcorrado@gmail.com>
2022-06-15 18:31:15 -0400

    updated packages

--- src/suggestions-server.js
+++ src/suggestions-server.js
@@ -1,29 +1,25 @@
 const SuggestionsService = {
-    getAllSuggestions(knex) {
-        return knex.select('*').from('suggestions')
-    },
-    insertSuggestion(knex, newSuggestion) {
-        return knex
-            .insert(newSuggestion)
-            .into('suggestions')
-            .returning('*')
-            .then(rows => {
-                return rows[0]
-            })
-    },
-    getById(knex, id) {
-        return knex.from('suggestions').select('*').where('id', id).first()
-    },
-    deleteSuggestion(knex, id) {
-        return knex('suggestions')
-            .where({ id })
-            .delete()
-    },
-    updateSuggestion(knex, id, newSuggestionFields) {
-        return knex('suggestions')
-            .where({ id })
-            .update(newSuggestionFields)
-    },
-}
-  
-  module.exports = SuggestionsService
+  getAllSuggestions(knex) {
+    return knex.select("*").from("suggestions");
+  },
+  insertSuggestion(knex, newSuggestion) {
+    return knex
+      .insert(newSuggestion)
+      .into("suggestions")
+      .returning("*")
+      .then((rows) => {
+        return rows[0];
+      });
+  },
+  getById(knex, id) {
+    return knex.from("suggestions").select("*").where("id", id).first();
+  },
+  deleteSuggestion(knex, id) {
+    return knex("suggestions").where({ id }).delete();
+  },
+  updateSuggestion(knex, id, newSuggestionFields) {
+    return knex("suggestions").where({ id }).update(newSuggestionFields);
+  },
+};
+
+module.exports = SuggestionsService;
